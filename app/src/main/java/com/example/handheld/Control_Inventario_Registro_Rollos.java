828162f2ca495e2af9e022c5f02d9ce8544b8450
MarshalLeeK <99676083+MarshalLeeK@users.noreply.github.com>
2023-06-08 13:21:40 -0500

    Se agrega el activity Registro rollos para inventario

---
+++ app/src/main/java/com/example/handheld/Control_Inventario_Registro_Rollos.java
@@ -0,0 +1,14 @@
+package com.example.handheld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Control_Inventario_Registro_Rollos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_control_inventario_registro_rollos);
+    }
+}
